f94b29aae569159c14d8520ec8cb89658eeeca12
Donovan Aikman <donovan@aikman.ca>
2021-11-07 15:43:15 -0800

    D first commit.

---
+++ src/components/Demographics.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Demographics() {
+  return (
+    <div className="Demograohics">
+        <header>
+            <h2>PLACEHOLDER</h2>
+        </header>
+    </div>
+  )
+}
+
